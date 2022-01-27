6a0c0b97f7985ee08b7d59bc8f24cfdc1008b79d
stru001 <struphp@sina.com>
2022-01-27 16:42:50 +0800

    change accesstokenentiy

--- src/Entity/AccessTokenEntity.php
+++ src/Entity/AccessTokenEntity.php
@@ -8,9 +8,8 @@ use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
 use League\OAuth2\Server\Entities\Traits\AccessTokenTrait;
 use League\OAuth2\Server\Entities\Traits\EntityTrait;
 use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
-use Stru\StruHyperfOauth\Model\AccessToken;
 
-class AccessTokenEntity extends AccessToken implements AccessTokenEntityInterface
+class AccessTokenEntity implements AccessTokenEntityInterface
 {
     use AccessTokenTrait,TokenEntityTrait,EntityTrait;
 }
